48a13bf95ee89d62a6f03c0d9fc506a4c5ef5cf4
Camilo Alvarez <cadorca@gmail.com>
2015-07-27 03:06:58 -0500

    actualización clases lab_22

--- taller_22/src/taller/Main.java
+++ taller_22/src/taller/Main.java
@@ -3,8 +3,8 @@ package taller;
 public class Main {
 
     public static void main(String[] args) {
-
-        System.out.println("taller");
+      Zoologico zoo = new Zoologico();
+      zoo.imprimirResultado();
 
     }
 
---
+++ taller_22/src/taller/Zoologico.java
@@ -0,0 +1,28 @@
+package taller;
+
+public class Zoologico {
+
+    public void ejecutarPruebasGenericidad() {
+      //TODO: Cree las jaulas y siga los pasos de la guía      
+    }
+  
+    public static String compararAnimales(Jaula<Animal> anim1, Jaula<Animal> anim2) {
+
+        //TODO: Modifique el método para hacer las pruebas
+        Animal mayor = null;
+
+        return "El animal más viejo entre " + null + "y " + null + "es " + mayor;
+    }
+
+    public void imprimirResultado() {
+
+        ejecutarPruebasGenericidad();
+        
+        //TODO: Modifique el resultado para que muestre el String resultante del metodo compararAnimales
+        System.out.println("Se van a comparar animales");
+        String compararAnimales = "";
+        System.out.println(compararAnimales);
+
+    }
+
+}
