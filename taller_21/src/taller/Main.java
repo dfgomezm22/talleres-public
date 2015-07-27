0d4e639bbc3f0161ec4ab6b8bd852e18a2c6a9e5
Camilo Alvarez <cadorca@gmail.com>
2015-07-27 01:44:25 -0500

    creación taller 2 parte 1

---
+++ taller_21/src/taller/Main.java
@@ -0,0 +1,15 @@
+package taller;
+
+import uniandes.cupi2.tienda.interfaz.*;
+
+public class Main{
+  
+  public static void main(String[] args){
+    
+        // Crea la ventana de la interfaz
+        InterfazTienda ventana = new InterfazTienda( );
+        ventana.setVisible( true );
+    
+  }
+  
+}
